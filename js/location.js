8d4d9f8f1102cba5a0e8140ea460351efd9eed46
Jake-woods <jake.13.5.2016@gmail.com>
2019-01-22 15:10:20 +0000

    Delete file

--- js/location.js
+++
@@ -1,5 +0,0 @@
-const lat = document.querySelector('input#latVal').val;
-const lon = document.querySelector('input#lonVal').val;
-const form = document.querySelector('form');
-
-form.addEventListener('submit', loadData);
